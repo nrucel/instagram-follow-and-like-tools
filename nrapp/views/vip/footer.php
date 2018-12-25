163f7ee7159120e5780147fa9551e47a760b0428
Nurullah Çelik <nurullahcelik@yandex.com>
2018-12-25 04:16:45 +0300

    Update footer.php

--- nrapp/views/vip/footer.php
+++ nrapp/views/vip/footer.php
@@ -15,190 +15,6 @@
     <script>
         var base_url = "<?=base_url()?>";
 
-        function userFollow(){
-
-            $('#link').attr('readonly', 'readonly');
-            $('#adet').attr('readonly', 'readonly');
-            $('#gonder').attr('disabled', 'disabled');
-            document.getElementById('gonder').innerHTML = "<i class='fas fa-circle-notch fa-spin'></i> işlem yapılıyor..";
-
-            var userName = document.getElementById("link").value;
-            var adet = document.getElementById("adet").value;
-
-            if(userName == "" || adet == ""){
-                $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> Lütfen boş alan bırakma !</div>');
-
-                $('#adet').removeAttr('readonly');
-                $('#link').removeAttr('readonly');
-                $('#gonder').prop("disabled", false);
-                document.getElementById('gonder').innerHTML = '<i class="fas fa-plus"></i>  Takipçi Gönder';
-                return;
-            }
-
-            
-
-            $.get( "https://www.instagram.com/"+userName, function( data ) {
-                data = JSON.parse(data.split("window._sharedData = ")[1].split(";<\/script>")[0]).entry_data.ProfilePage[0].graphql;
-                console.log(data.user);
-
-                if(data.user){
-
-                    if(data.user.is_private == false){
-
-                        $.ajax({
-                            type: 'post',
-                            url: base_url+'vip/takipciGonder',
-                            dataType: 'json',
-                            data: "link="+data.user.username+"&igid="+data.user.id+"&adet="+adet,
-                            success: function(result) {
-                                console.log(result);
-                                if(result.status == "ok") {
-
-                                    $('#sonuc').html('<div class="panel-body" style="background:green;color:#fff;"><i class="fas fa-check" style="font-weight=bold; font-size:15px;"></i> Başarılı.. '+result.basarili+' takipçi gönderildi.</div>');
-                                } else {
-
-                                    $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> '+result.message+'</div>');
-                                }
-
-                                $('#adet').removeAttr('readonly');
-                                $('#link').removeAttr('readonly');
-                                $('#gonder').prop("disabled", false);
-                                document.getElementById('gonder').innerHTML = '<i class="fas fa-plus"></i>  Takipçi Gönder';
-
-                                }
-                        });
-
-                    } else {
-
-                        $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> Hesap gizli. Lütfen hesabınızı herkese açık yapınız.</div>');
-                        $('#adet').removeAttr('readonly');
-                        $('#link').removeAttr('readonly');
-                        $('#gonder').prop("disabled", false);
-                        document.getElementById('gonder').innerHTML = '<i class="fas fa-plus"></i>  Takipçi Gönder';
-
-                }
-
-                } else{
-
-                    $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> hatalı kullanıcı adı girdiniz kontrol edip tekrar deneyin.</div>');
-                    $('#adet').removeAttr('readonly');
-                    $('#link').removeAttr('readonly');
-                    $('#gonder').prop("disabled", false);
-                    document.getElementById('gonder').innerHTML = '<i class="fas fa-plus"></i>  Takipçi Gönder';
-                }
-                
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-
-                if(jqXHR.status == 404) {
-                    $('#sonuc').html('<div style="background:#c50f0f;color:#fff;margin-top:10px;padding:10px"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> hatalı kullanıcı adı girdiniz kontrol edip tekrar deneyin.</div>');
-                    $('#link').removeAttr('readonly');
-                    $('#adet').removeAttr('readonly');
-                    $('#gonder').prop("disabled", false);
-                    document.getElementById('gonder').innerHTML = '<i class="fas fa-search-plus"></i>  Kullanıcıyı Bul';
-                } else {
-                    $('#sonuc').html('<div style="background:#c50f0f;color:#fff;margin-top:10px;padding:10px"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> hatalı kullanıcı adı girdiniz kontrol edip tekrar deneyin</div>');
-
-                $('#adet').removeAttr('readonly');
-                $('#link').removeAttr('readonly');
-                $('#gonder').prop("disabled", false);
-                document.getElementById('gonder').innerHTML = '<i class="fas fa-search-plus"></i>  Kullanıcıyı Bul';
-                }
-              })
-
-        }
-
-
-        function apiEkle(){
-
-            $('#link').attr('readonly', 'readonly');
-            $('#adet').attr('readonly', 'readonly');
-            $('#service').attr('readonly', 'readonly');
-            $('#gonder').attr('disabled', 'disabled');
-            document.getElementById('gonder').innerHTML = "<i class='fas fa-circle-notch fa-spin'></i> işlem yapılıyor..";
-
-            var userName = document.getElementById("link").value;
-            var adet = document.getElementById("adet").value;
-            var service = document.getElementById("service").value;
-
-            if(userName == "" || adet == "" || service == ""){
-                $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> Lütfen boş alan bırakma !</div>');
-
-                $('#adet').removeAttr('readonly');
-                $('#link').removeAttr('readonly');
-                $('#gonder').prop("disabled", false);
-                document.getElementById('gonder').innerHTML = '<i class="fas fa-plus"></i>  Sipariş Ekle';
-                return;
-            }
-
-
-                        $.ajax({
-                            type: 'post',
-                            url: base_url+"/api",
-                            dataType: 'json',
-                            data: "link="+userName+"&quantity="+adet+"&service="+service+"&action=add&key=<?=$this->config->item("apiKey");?>",
-                            success: function(result) {
-                                console.log(result);
-                                if(result.order) {
-
-                                    $('#sonuc').html('<div class="panel-body" style="background:green;color:#fff;"><i class="fas fa-check" style="font-weight=bold; font-size:15px;"></i> <b>#ID '+result.order+'</b> sipariş eklendi.</div>');
-                                } else {
-
-                                    $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> '+result.error+'</div>');
-                                }
-
-                                $('#adet').removeAttr('readonly');
-                                $('#link').removeAttr('readonly');
-                                $('#service').removeAttr('readonly');
-                                $('#gonder').prop("disabled", false);
-                                document.getElementById('gonder').innerHTML = '<i class="fas fa-plus"></i>  Sipariş Ekle';
-
-                                }
-                
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-
-                $('#sonuc').html('<div style="background:#c50f0f;color:#fff;margin-top:10px;padding:10px"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> hatalı kullanıcı adı girdiniz kontrol edip tekrar deneyin</div>');
-
-                $('#adet').removeAttr('readonly');
-                $('#link').removeAttr('readonly');
-                $('#service').removeAttr('readonly');
-                $('#gonder').prop("disabled", false);
-                document.getElementById('gonder').innerHTML = '<i class="fas fa-plus"></i>  Sipariş Ekle';
-              })
-
-        }
-
-
-
-        function userLike(){
-
-            var link = document.getElementById("link").value;
-            var adet = document.getElementById("adet").value;
-            if(link == "" || adet == ""){
-                $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> Lütfen boş alan bırakma !</div>');
-                return;
-            }
-
-            $.get( link, function( data ) {
-                data = JSON.parse(data.split("window._sharedData = ")[1].split(";<\/script>")[0]).entry_data.PostPage[0].graphql;
-                console.log(data.shortcode_media);
-
-                if(data )
-                if(data.media_id !== ""){
-                    $('#sonuc').html('<div class="panel-body" style="background:green;color:#fff;">'+data.media_id+'</div>');
-                } else{
-
-                    $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> hatalı link girdiniz kontrol edip tekrar deneyin. Unutmayın profil gizli ise resim çekilemez. !</div>');
-                }
-                
-            })
-            .fail(function() {
-                $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> hatalı link girdiniz kontrol edip tekrar deneyin. Unutmayın profil gizli ise resim çekilemez. !</div>');
-              })
-        }
-
-
     function blogEkle(){
 
         $('#gonder').attr('disabled', 'disabled');
@@ -256,13 +72,14 @@
         var blogAnahtar  = document.getElementById("blogAnahtar").value;
         var blogUrl      = document.getElementById("blogUrl").value;
         var blogResim    = document.getElementById("blogResim").value;
+        var zaman        = document.getElementById("zaman").value;
         var blogDetay    = theEditor.getData();
 
         if(blogAdi == "" || blogAciklama == "" || blogAnahtar == "" || blogUrl == "" || blogDetay == ""){
             $('#sonuc').html('<div class="panel-body" style="background:#c50f0f;color:#fff;"><i class="fas fa-exclamation" style="font-weight=bold; font-size:15px;"></i> Lütfen boş alan bırakma !</div>');
 
             $('#gonder').prop("disabled", false);
-                document.getElementById('gonder').innerHTML = 'Blog Ekle';
+                document.getElementById('gonder').innerHTML = 'Blog Düzenle';
                 return;
         }
 
@@ -274,7 +91,7 @@
             type: 'post',
             url: window.location.href,
             dataType: 'json',
-            data: "blogAdi="+blogAdi+"&blogAciklama="+blogAciklama+"&blogAnahtar="+blogAnahtar+"&blogUrl="+blogUrl+"&blogResim="+blogResim+"&blogDetay="+blogDetay,
+            data: "blogAdi="+blogAdi+"&blogAciklama="+blogAciklama+"&blogAnahtar="+blogAnahtar+"&blogUrl="+blogUrl+"&blogResim="+blogResim+"&blogDetay="+blogDetay+"&zaman="+zaman,
             success: function(result) {
                 console.log(result);
                 if(result.status == "ok") {
@@ -286,7 +103,7 @@
                 }
 
                 $('#gonder').prop("disabled", false);
-                document.getElementById('gonder').innerHTML = 'blog Ekle';
+                document.getElementById('gonder').innerHTML = 'blog Düzenle';
 
                 }
         });
